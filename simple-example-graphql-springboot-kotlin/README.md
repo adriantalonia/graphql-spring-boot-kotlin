bc6b00c2808661d0d0f8fbca8b091dd7dc1b16da
Adrian <adrian.talonia11@gmail.com>
2023-04-04 23:01:15 -0600

    first commit

---
+++ simple-example-graphql-springboot-kotlin/README.md
@@ -0,0 +1,19 @@
+# GraphQl With Spring Boot Kotlin
+
+Different projects about:
+- Graphql with spring boot and how to create graphql application with easy steps
+
+## Authors
+
+- Adrian Talonia Ramos [Java FullStack Developer] [@adriantalonia](https://github.com/adriantalonia)
+
+
+## Technology Stack
+- Java 17
+- Springboot 3
+- Kotlin 1.7
+
+
+
+## Development Prerequisites
+- Intellij IDEA
---
+++ simple-example-graphql-springboot-kotlin/src/main/kotlin/com/graph/example/query/PostResolver.kt
@@ -0,0 +1,26 @@
+package com.graph.example.query
+
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+import java.util.*
+
+@Controller
+class PostResolver {
+
+    @QueryMapping
+    fun getPosts(): List<PostDto> {
+        return listOf(
+            PostDto(
+                UUID.randomUUID(),
+                "title test",
+                "description test"
+            )
+        )
+    }
+}
+
+data class PostDto(
+    val id: UUID,
+    val title: String,
+    val description: String
+)
